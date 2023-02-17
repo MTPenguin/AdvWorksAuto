eac0faf7c2896bd65ef4c6d0a2ad74c64593683c
MTPenguinAction <MTPenguinAction@youremail.com>
2023-02-17 12:03:20 -0700

    Try new project

---
+++ migrations/U002_20230217190132__BMac.sql
@@ -0,0 +1,36 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating schemas'
+GO
+IF SCHEMA_ID(N'flyway') IS NULL
+EXEC sp_executesql N'CREATE SCHEMA [flyway]
+AUTHORIZATION [dbo]'
+GO
+PRINT N'Dropping extended properties'
+GO
+EXEC sp_dropextendedproperty N'MS_Description', 'SCHEMA', N'SalesLT', 'TABLE', N'Customer', 'CONSTRAINT', N'DF_Customer_ModifiedDate'
+GO
+EXEC sp_dropextendedproperty N'MS_Description', 'SCHEMA', N'SalesLT', 'TABLE', N'Customer', 'CONSTRAINT', N'DF_Customer_NameStyle'
+GO
+EXEC sp_dropextendedproperty N'MS_Description', 'SCHEMA', N'SalesLT', 'TABLE', N'Customer', 'CONSTRAINT', N'DF_Customer_rowguid'
+GO
+EXEC sp_dropextendedproperty N'MS_Description', 'SCHEMA', N'SalesLT', 'TABLE', N'Customer', 'INDEX', N'PK_Customer_CustomerID'
+GO
+EXEC sp_dropextendedproperty N'MS_Description', 'SCHEMA', N'SalesLT', 'TABLE', N'SalesOrderHeader', 'CONSTRAINT', N'FK_SalesOrderHeader_Customer_CustomerID'
+GO
+PRINT N'Dropping foreign keys from [SalesLT].[SalesOrderHeader]'
+GO
+ALTER TABLE [SalesLT].[SalesOrderHeader] DROP CONSTRAINT [FK_SalesOrderHeader_Customer_CustomerID]
+GO
+PRINT N'Altering [SalesLT].[Customer]'
+GO
+ALTER TABLE [SalesLT].[Customer] DROP
+COLUMN [New2]
+GO
+PRINT N'Altering extended properties'
+GO
+EXEC sp_updateextendedproperty N'MS_Description', N'Clustered index created by a primary key constraint.', 'SCHEMA', N'SalesLT', 'TABLE', N'Customer', 'CONSTRAINT', N'PK_Customer_CustomerID'
+GO
+
---
+++ migrations/V002_20230217190132__BMac.sql
@@ -0,0 +1,35 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Altering [SalesLT].[Customer]'
+GO
+ALTER TABLE [SalesLT].[Customer] ADD
+[New2] [nchar] (10) NULL
+GO
+PRINT N'Adding foreign keys to [SalesLT].[SalesOrderHeader]'
+GO
+ALTER TABLE [SalesLT].[SalesOrderHeader] ADD CONSTRAINT [FK_SalesOrderHeader_Customer_CustomerID] FOREIGN KEY ([CustomerID]) REFERENCES [SalesLT].[Customer] ([CustomerID])
+GO
+PRINT N'Altering extended properties'
+GO
+EXEC sp_updateextendedproperty N'MS_Description', N'Primary key (clustered) constraint', 'SCHEMA', N'SalesLT', 'TABLE', N'Customer', 'CONSTRAINT', N'PK_Customer_CustomerID'
+GO
+PRINT N'Creating extended properties'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Default constraint value of GETDATE()', 'SCHEMA', N'SalesLT', 'TABLE', N'Customer', 'CONSTRAINT', N'DF_Customer_ModifiedDate'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Default constraint value of 0', 'SCHEMA', N'SalesLT', 'TABLE', N'Customer', 'CONSTRAINT', N'DF_Customer_NameStyle'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Default constraint value of NEWID()', 'SCHEMA', N'SalesLT', 'TABLE', N'Customer', 'CONSTRAINT', N'DF_Customer_rowguid'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Clustered index created by a primary key constraint.', 'SCHEMA', N'SalesLT', 'TABLE', N'Customer', 'INDEX', N'PK_Customer_CustomerID'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Foreign key constraint referencing Customer.CustomerID.', 'SCHEMA', N'SalesLT', 'TABLE', N'SalesOrderHeader', 'CONSTRAINT', N'FK_SalesOrderHeader_Customer_CustomerID'
+GO
+PRINT N'Dropping schemas'
+GO
+IF SCHEMA_ID(N'flyway') IS NOT NULL
+DROP SCHEMA [flyway]
+GO
+
