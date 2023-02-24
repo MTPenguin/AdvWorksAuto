0fdd53a07c36f71d9395b440ed129388c9171921
MTPenguin <mtpenguin@gmail.com>
2023-02-24 11:03:46 -0700

    Reset Flyway project

--- schema-model/Extended Properties/MS_Description.sql
+++
@@ -1,2 +0,0 @@
-EXEC sp_addextendedproperty N'MS_Description', N'AdventureWorksLT 2012 Sample OLTP Database', NULL, NULL, NULL, NULL, NULL, NULL
-GO
--- schema-model/Security/Schemas/SalesLT.sql
+++
@@ -1,5 +0,0 @@
-CREATE SCHEMA [SalesLT]
-AUTHORIZATION [dbo]
-GO
-EXEC sp_addextendedproperty N'MS_Description', N'Contains objects related to products, customers, sales orders, and sales territories.', 'SCHEMA', N'SalesLT', NULL, NULL, NULL, NULL
-GO
